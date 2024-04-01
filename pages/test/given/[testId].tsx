54e8f335a84203370c9cccd380ce032c53bd816f
Rajat Nai <rajatnai49@gmail.com>
2024-04-01 23:08:17 +0530

    add test-monitor

--- pages/test/given/[testId].tsx
+++ pages/test/given/[testId].tsx
@@ -5,86 +5,86 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const UserTestStatsPage = () => {
-    const session = useSession().data;
-    const [testStats, setTestStats] = useState<any>();
-    const router = useRouter();
-    const { testId } = router.query;
-    const [questionAttempts, setQuestionAttempts] = useState<questionAttempt[]>([]);
-    const [loading, setLoading] = useState(true);
+  const session = useSession().data;
+  const [testStats, setTestStats] = useState<any>();
+  const router = useRouter();
+  const { testId } = router.query;
+  const [questionAttempts, setQuestionAttempts] = useState<questionAttempt[]>([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function fetchTestStats() {
-            try {
-                if (!testId) return; // Ensure testId is available before making the request
-                console.log("testId:", testId);
-                console.log("userId:", session?.user.id);
+  useEffect(() => {
+    async function fetchTestStats() {
+      try {
+        if (!testId) return; // Ensure testId is available before making the request
+        console.log("testId:", testId);
+        console.log("userId:", session?.user.id);
 
-                const res = await axios.get("/api/stats/users/fetchUserTestStat/", { params: { testId: testId, userId: session?.user.id } });
-                const questionAttempts: questionAttempt[] = [];
+        const res = await axios.get("/api/stats/users/fetchUserTestStat/", { params: { testId: testId, userId: session?.user.id } });
+        const questionAttempts: questionAttempt[] = [];
 
-                for (const question of res.data.questions) {
-                    const attempts = question.attempts;
-                    const correctAttempts = question.correctAttempts;
-                    const incorrectAttempts = attempts - correctAttempts;
-                    questionAttempts.push({ questionId: question._id, attempts, correctAttempts, incorrectAttempts });
-                }
-
-                console.log(res.data);
-                setTestStats(res.data);
-                setQuestionAttempts(questionAttempts);
-                setLoading(false);
-            }
-            catch (error) {
-                console.error("Error fetching test stats:", error);
-            }
+        for (const question of res.data.questions) {
+          const attempts = question.attempts;
+          const correctAttempts = question.correctAttempts;
+          const incorrectAttempts = attempts - correctAttempts;
+          questionAttempts.push({ questionId: question._id, attempts, correctAttempts, incorrectAttempts });
         }
 
-        if (session) {
-            fetchTestStats(); // Call fetchTestStats inside useEffect
+        console.log(res.data);
+        setTestStats(res.data);
+        setQuestionAttempts(questionAttempts);
+        setLoading(false);
+      }
+      catch (error) {
+        console.error("Error fetching test stats:", error);
+      }
+    }
+
+    if (session) {
+      fetchTestStats(); // Call fetchTestStats inside useEffect
+    }
+  }, [session, testId]); // Add testId as a dependency
+
+  return (
+    <>
+      <ul className="text-xl px-40 mt-40">
+        <li>
+          Bar Chart: Compare the average score for each question. Each bar would represent a question, and the height of the bar would represent the average score.
+        </li>
+        <li>
+          Line Chart: Show the average time taken to answer each question. Each line would represent a question, and the points on the line would indicate the average time.
+        </li>
+        <li>
+          Pie Chart: Pie chart to show the distribution of correct and incorrect attempts for each question. Each slice of the pie would represent either a correct or incorrect attempt.
+        </li>
+        <li>
+          Scatter Plot: To visualize the relationship between the average time taken and the average score for each question. Each point on the plot would represent a question, with the x-axis representing the average time and the y-axis representing the average score.
+        </li>
+      </ul>
+
+      <div className="w-screen grid grid-cols-3 grid-rows-3 mt-40 px-40 items-center">
+        {
+          loading ? <h1>Loading...</h1> :
+            questionAttempts.map((questionAttempt, index) => {
+              return (
+                <DoghnutChart key={index} data={{
+                  labels: ["Attempted", "Correct", "Incorrect"],
+                  data: [questionAttempt.attempts, questionAttempt.correctAttempts, questionAttempt.incorrectAttempts],
+                  name: `Question ${index + 1}`
+                }} />
+              );
+            }
+            )
         }
-    }, [session, testId]); // Add testId as a dependency
-
-    return (
-        <>
-            <ul className="text-xl px-40 mt-40">
-                <li>
-                    Bar Chart: Compare the average score for each question. Each bar would represent a question, and the height of the bar would represent the average score.
-                </li>
-                <li>
-                    Line Chart: Show the average time taken to answer each question. Each line would represent a question, and the points on the line would indicate the average time.
-                </li>
-                <li>
-                    Pie Chart: Pie chart to show the distribution of correct and incorrect attempts for each question. Each slice of the pie would represent either a correct or incorrect attempt.
-                </li>
-                <li>
-                    Scatter Plot: To visualize the relationship between the average time taken and the average score for each question. Each point on the plot would represent a question, with the x-axis representing the average time and the y-axis representing the average score.
-                </li>
-            </ul>
-
-            <div className="w-screen flex mt-40 px-40 items-center">
-                {
-                    loading ? <h1>Loading...</h1> :
-                        questionAttempts.map((questionAttempt, index) => {
-                            return (
-                                <DoghnutChart key={index} data={{
-                                    labels: ["Attempted", "Correct", "Incorrect"],
-                                    data: [questionAttempt.attempts, questionAttempt.correctAttempts, questionAttempt.incorrectAttempts],
-                                    name: `Question ${index + 1}`
-                                }} />
-                            );
-                        }
-                        )
-                }
-            </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 interface questionAttempt {
-    questionId: string;
-    attempts: number;
-    correctAttempts: number;
-    incorrectAttempts: number;
+  questionId: string;
+  attempts: number;
+  correctAttempts: number;
+  incorrectAttempts: number;
 }
 
 export default UserTestStatsPage;
--- pages/test_monitor/index.tsx
+++ pages/test_monitor/index.tsx
@@ -1,12 +1,55 @@
-import { useEffect, useState } from 'react';
 import CryptoJS from 'crypto-js';
 import TestTimer from '@/components/TestMonitor/TestTimer';
 import Questions from '@/components/Monitor/Questions';
 import AnswerButtons from '@/components/Monitor/AnswerButton';
 import QuestionListButtons from '@/components/Monitor/QuestionListButtons';
 import { useSession } from 'next-auth/react';
+import React, { useState, useEffect, useRef } from 'react';
+import Webcam from 'react-webcam';
+import io from 'socket.io-client';
+import axios from 'axios';
+
+const socket = io('http://localhost:5000');
 
 const TestMonitor = () => {
+  const webcamRef = useRef(null);
+  const [faceCount, setFaceCount] = useState(0);
+
+  useEffect(() => {
+    // Set up a timer to capture frames periodically
+    const timer = setInterval(() => {
+      captureFrame();
+    }, 1000); // Adjust the interval (in milliseconds) as needed
+
+    // Clean up the timer when the component unmounts
+    return () => clearInterval(timer);
+  }, []);
+
+  const captureFrame = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    const imageData = imageSrc.split(',')[1]; // Extract base64 data from data URL
+    const byteCharacters = atob(imageData); // Decode base64 to byte characters
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers); // Convert to Uint8Array (bytes)
+    socket.emit('frame', byteArray); // Send bytes data to backend
+  };
+
+  useEffect(() => {
+    // Listen for face count updates from the server
+    socket.on('face_count', count => {
+      setFaceCount(count);
+    });
+
+    // Clean up socket listener when the component unmounts
+    return () => {
+      socket.off('face_count');
+    };
+  }, []);
+
+
   const session = useSession().data
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
   const [testData, setTestData] = useState<any>();
@@ -42,13 +85,58 @@ const TestMonitor = () => {
     setCurrentQuestionIndex((prevState: number) => prevState == 0 ? prevState : prevState - 1);
   }
 
-  const handleSubmitQuiz = () => {
-    const testId = session?.user.id;
+  const prepareDataForSubmission = () => {
+    const questionsData = localStorage.getItem('userQuestionData')
+
+    const userQuestionData = JSON.parse(questionsData);
+
+    // Extract required information from userQuestionData
+    const userQuestionStats = userQuestionData.map((item) => ({
+      question_id: item.id,
+      userAnswer: item.userAnswer,
+      time: item.timestamps.reduce((acc, cur) => acc + cur, 0), // Calculate total time
+    }));
+
+    return userQuestionStats;
+  };
+
+  const handleSubmitQuiz = async () => {
+    const owner = session?.user.id;
+    const testId = testData._id;
+    const startedTime = localStorage.getItem('startTimestamp')
+    const startDate = new Date(parseInt(startedTime, 10));
+
+    const currentDate = new Date();
+    const timeDifference = currentDate.getTime() - startDate.getTime();
+
+    const timeDiffInMinutes = Math.floor(timeDifference / (1000 * 60));
+    const userQuestionStats = prepareDataForSubmission();
+
+    const data = {
+      owner: owner,
+      testId: testId,
+      time: timeDiffInMinutes,
+      userQuestionStats: userQuestionStats,
+    }
+
+    try {
+      const res = await axios.post("api/test/complete", data)
+      console.log(res)
+    }
+    catch (e) {
+      console.log("There is error in the request")
+    }
 
   }
 
   return (
     <>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+      />
+      <p>Number of Faces: {faceCount}</p>
       {/* Your TestMonitor JSX content */}
       {startTime &&
         <TestTimer startTime={startTime} duration={testData.minutes} />
